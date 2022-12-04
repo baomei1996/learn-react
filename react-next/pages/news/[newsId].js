f2728495f3d5ccbacf1b2f4175d466f095a2f60f
baomei1996 <qhal4686@gmail.com>
2022-12-04 22:08:48 +0900

    Sec23: useRouter를 활용한 동적 routing

--- react-next/pages/news/[newsId].js
+++ react-next/pages/news/[newsId].js
@@ -1,6 +1,14 @@
+import { useRouter } from "next/router"
+
 // our-domain.com/news/something-details
 
 function DetailPage() {
+  const router = useRouter();
+
+  // send a request to the backend API
+  // to fetch the news item with newsId
+
+  const newsId = router.query.newsId;
     return <h1>The Details Page</h1>
   }
   
