db6d22fe65d6922f719ff62d448831b77b9ee573
baomei1996 <qhal4686@gmail.com>
2022-12-04 22:18:14 +0900

    Sec23: next Link를 활용한 페이지간 이동 처리

--- react-next/pages/news/index.js
+++ react-next/pages/news/index.js
@@ -1,7 +1,24 @@
+import Link from "next/link"
+import { Fragment } from "react"
+
 // our-domain.com/news
 
 function NewsPage() {
-    return <h1>The News Page</h1>
+    return <Fragment>
+       <h1>The News Page</h1>
+       <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-greate-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">
+            Something Else
+          </Link>
+        </li>
+       </ul>
+    </Fragment>
   }
   
   export default NewsPage
