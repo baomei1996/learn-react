03e84325f6ff835787212fc523ee57c0d28ec3f1
baomei1996 <qhal4686@gmail.com>
2022-12-11 23:02:53 +0900

    Feat: MeetupDetail component 만들기
    module.css 로 독립적으로 스타일링 하기

---
+++ nextjs-project/pages/[meetupId]/index.js
@@ -0,0 +1,10 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+    return (
+        <MeetupDetail image="https://images.prismic.io/mystique/5d7c09b9-40e5-4254-ae1c-2c1cb59aa898_IMG3.jpg?auto=compress,format"
+            title="A First Meetup" address="Some Street 5, Some City" description="The Meetup description"/>
+    )
+}
+
+export default MeetupDetails;
