35fd3f13f764a4a44c44d6957999e0f959abeccd
baomei1996 <qhal4686@gmail.com>
2023-07-02 22:44:43 +0900

    Feat: 각각의 state로 관리하는 것으로 변경

--- react-basic/react-start/src/components/new-expense/ExpenseForm.js
+++ react-basic/react-start/src/components/new-expense/ExpenseForm.js
@@ -2,37 +2,37 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [enteredAmount, setEnteredAmount] = useState(0);
-  //   const [enteredDate, setEnteredDate] = useState(new Date());
-  const [useInput, setUserInput] = useState({
-    title: "",
-    amount: "",
-    date: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState(0);
+  const [enteredDate, setEnteredDate] = useState(new Date());
+  //   const [useInput, setUserInput] = useState({
+  //     title: "",
+  //     amount: "",
+  //     date: "",
+  //   });
 
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
+    setEnteredTitle(event.target.value);
     // setUserInput({ ...useInput, title: event.target.value });
-    setUserInput((prevState) => {
-      return { ...prevState, title: event.target.value };
-    });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, title: event.target.value };
+    // });
   };
 
   const amountChangeHandler = (event) => {
-    // setEnteredAmount(event.target.value);
+    setEnteredAmount(event.target.value);
     // setUserInput({ ...useInput, amount: event.target.value });
-    setUserInput((prevState) => {
-      return { ...prevState, amount: event.target.value };
-    });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, amount: event.target.value };
+    // });
   };
 
   const dateChangeHandler = (event) => {
-    // setEnteredDate(event.target.value);
+    setEnteredDate(event.target.value);
     // setUserInput({ ...useInput, date: event.target.value });
-    setUserInput((prevState) => {
-      return { ...prevState, date: event.target.value };
-    });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, date: event.target.value };
+    // });
   };
 
   return (
